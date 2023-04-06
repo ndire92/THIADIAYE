65c1534e69323756e3c4ccc97b77c0388a3621f6
ndir92 <mamadoundir2@gmail.com>
2023-04-06 03:28:52 +0000

    first commit

--- Stud/settings.py
+++ Stud/settings.py
@@ -135,5 +135,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'app.CustomUser'
+AUTH_USER_MODEL = 'auth.User'
